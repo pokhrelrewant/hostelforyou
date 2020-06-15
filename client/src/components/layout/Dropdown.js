84f9bf868029d1eccafffdec5c8b4a49e3b28da8
mynameistakenisalsotaken <niravnikhil@gmail.com>
2020-06-15 13:03:51 +0545

    newsearch is better

--- client/src/components/layout/Dropdown.js
+++ client/src/components/layout/Dropdown.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
 
 class Dropdown extends Component {
-  //On the change event for the select box pass the selected value back to the parent
   handleChange = event => {
     let selectedValue = event.target.value;
     this.props.onSelectChange(selectedValue);
   };
 
   render() {
+    let i = 0;
     let arrayOfData = this.props.arrayOfData;
     let options = arrayOfData.map(data => (
-      <option key={data.id} value={data.id}>
+      <option key={i++} value={data.name}>
         {data.name}
       </option>
     ));
 
     return (
       <select
-        name='customSearch'
         className='custom-search-select form-control'
         onChange={this.handleChange}
       >
-        <option>Select Item</option>
+        <option id='-1' value='-1'>Select Item</option>
         {options}
       </select>
     );
--- client/src/components/layout/NewSearch.js
+++ client/src/components/layout/NewSearch.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
 import HostelCard from "../hostelCard/HostelCard";
@@ -6,143 +6,90 @@ import retriever from "../../actions/retriever";
 
 import Dropdown from "./Dropdown";
 import Navbar from "./Navbar";
+import "./search.css"
+
+const arrayOfData = [
+  { name: "Pulchowk" },
+  { name: "Patan" },
+  { name: "Thapathali" },
+  { name: "Ratnapark" },
+  { name: "Babarmahal" },
+];
 
 const NewSearch = ({ retriever }) => {
-  const [formData, setFormData] = useState({
-    name: "",
-    nearbyInstitutions: "",
-    location: "",
-    buttonload: false,
-  });
-
-  const arrayOfData = [
-    {
-      id: 1,
-      name: "Pulchowk",
-    },
-    {
-      id: 2,
-      name: "Patan",
-    },
-    {
-      id: 3,
-      name: "Thapathali",
-    },
-    {
-      id: 4,
-      name: "Ratnapark",
-    },
-    {
-      id: 5,
-      name: "Babarmahal",
-    },
-  ];
-
-  const { name, nearbyInstitutions, location, buttonload } = formData;
-
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  const onInstitutionChange = (nearbyInstitutions) => {
-    console.log(arrayOfData[nearbyInstitutions - 1]);
-    setFormData({
-      ...formData,
-      nearbyInstitutions: arrayOfData[nearbyInstitutions - 1].name,
-    });
-  };
-
-  const onLocationChange = (location) => {
-    console.log(arrayOfData[location - 1]);
-    setFormData({ ...formData, location: arrayOfData[location - 1].name });
-  };
+  let [nearbyInstitutions, setNearbyInstitutions]=useState("");
+  let [location, setLocation]=useState("");
+  let [loading, setLoading]=useState(false);
+  let searchText = useRef();
 
   const onSubmit = async (e) => {
-    setFormData({ ...formData, buttonload: true });
-    console.log(formData);
-    await retriever(name, nearbyInstitutions, location);
-    setFormData({ ...formData, buttonload: false });
-  };
-
-  const showData = () => {
-    return <HostelCard />;
+    e.preventDefault();
+    setLoading(true);
+    console.log({s:searchText.value, nearbyInstitutions, location, loading})
+    await retriever(searchText.value, nearbyInstitutions, location);
+    setLoading(false)
   };
 
   return (
     <Fragment>
       <Navbar />
-
       <div>
         <div className='container container-sm containerNirav my-5'>
           <img
             src={"/bermuda-searching.png"}
             alt='searching'
-            style={{
-              margin: "0 auto",
-              marginBottom: "50px",
-              display: "block",
-            }}
-            className='responsive'
+            className='responsive searchImg'
           />
           <h3 className='mb-2'>Search Page </h3>
-          <div className='input-group'>
-            <input
-              value={name}
-              name='name'
-              onChange={(e) => onChange(e)}
-              className='form-control '
-              type='search'
-              placeholder='Search for the hostel'
-              aria-label='Search'
-            />
-            <button
-              onClick={onSubmit}
-              className='btn btn-outline-success ml-xl-2'
-              type='submit'
-            >
-              {buttonload ? (
-                <div
-                  className='spinner-border m-1'
-                  style={{ width: "15px", height: "15px" }}
-                  role='status'
+            <form>
+              <div className='input-group'>
+                <input
+                  className='form-control '
+                  placeholder='Search for the hostel'
+                  ref={n=>searchText=n}
+                />
+                <button
+                  onClick={onSubmit}
+                  className='btn btn-outline-success ml-xl-2'
+                  type='submit'
                 >
-                  <span className='sr-only'>Loading...</span>
+                  {loading ? (
+                    <div
+                      className='spinner-border m-1'
+                      style={{ width: "15px", height: "15px" }}
+                      role='status'
+                    >
+                      <span className='sr-only'>Loading...</span>
+                    </div>
+                  ) : (
+                    "Search"
+                  )}
+                </button>
+              </div>
+
+              <div className='row'>
+                <div className='col'>
+                  <label>Nearby Institutions</label>
+                  <Dropdown
+                    arrayOfData={arrayOfData}
+                    onSelectChange={e => setNearbyInstitutions(e)}
+                  />
                 </div>
-              ) : (
-                "Search"
-              )}
-            </button>
-          </div>
-          <br />
-          <div className='row'>
-            <div className='col'>
-              <label>Nearby Institutions</label>
-              <Dropdown
-                arrayOfData={arrayOfData}
-                onSelectChange={(e) => onInstitutionChange(e)}
-              />{" "}
-              <br />
-              <br />
-            </div>
-            <div className='col'>
-              <label>Hostel Address</label>
-              <Dropdown
-                arrayOfData={arrayOfData}
-                name='location'
-                onSelectChange={(e) => onLocationChange(e)}
-              />{" "}
-              <br />
-              <br />
-            </div>
-          </div>
-          <div className='card-deck'>{showData()}</div>
+                <div className='col'>
+                  <label>Hostel Address</label>
+                  <Dropdown
+                    arrayOfData={arrayOfData}
+                    name='location'
+                    onSelectChange={e => setLocation(e)}
+                  />
+                </div>
+              </div>
+            </form>
+          <div className='card-deck'><HostelCard/></div>
         </div>
       </div>
     </Fragment>
   );
 };
 
-const mapStateToProps = (state) => ({
-  loading: state.retriever.loading,
-});
-
-export default connect(mapStateToProps, { setAlert, retriever })(NewSearch);
+export default connect(_=>({}), { setAlert, retriever })(NewSearch);
